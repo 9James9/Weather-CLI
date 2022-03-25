372cdbe709ccd455d00f57e02c2bd600d294c6a0
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-25 13:34:40 -0500

    updated script.py

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[56]:
+# In[93]:
 
 
 import requests
 import json
 
 
-# In[57]:
+# In[94]:
 
 
 def get_response():
@@ -17,47 +17,34 @@ def get_response():
     return response
 
 
-# In[58]:
-
-
-# response = requests.get("https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=eb40cd8fe7b3bb3460776fbee82754b2")
-
-
-# In[59]:
+# In[96]:
 
 
 response = get_response()
 
 
-# In[60]:
+# In[97]:
 
 
 text = json.dumps(response.json(),sort_keys=True, indent=4)
-print(text)
+# print(text)
 
 
-# In[61]:
-
-
-test = response.json()['main']['temp']
-print(test)
-
-
-# In[62]:
+# In[98]:
 
 
 def convert_k_to_c(kelvin):
     return kelvin - 273.15
 
 
-# In[63]:
+# In[99]:
 
 
 def convert_c_to_f(celcius):
     return (celcius * 1.8) + 32
 
 
-# In[64]:
+# In[100]:
 
 
 def convert_k_to_f(kelvin):
@@ -66,8 +53,19 @@ def convert_k_to_f(kelvin):
     return output
 
 
-# In[65]:
+# In[109]:
 
 
-print(convert_k_to_f(response.json()['main']['temp']))
+def print_temps():
+    print("Current weather for " + response.json()['name'])
+    print("Current temperature: " + str(round(convert_k_to_f(response.json()['main']['temp']),2)) + " F")
+    feels_like = round(convert_k_to_f(response.json()['main']['feels_like']),2)
+    print("Feels like: " + str(feels_like) + " F")
+    humidity = response.json()['main']['humidity']
+    print("Humidity: " + str(humidity))
+    high_temp = round(convert_k_to_f(response.json()['main']['temp_max']))
+    print("High: " + str(round(high_temp,2)) + " F")
+    low_temp = round(convert_k_to_f(response.json()['main']['temp_min']))
+    print("Low: " + str(round(low_temp,2)) + " F")
+print_temps()
 
